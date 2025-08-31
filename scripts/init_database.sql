7aa5819371237f520091454d7bb962b01f5c4156
MohibullahTheProblemSolver <mdmohibullahbapari@gmail.com>
2025-08-31 18:11:41 +0600

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,54 @@
+/*
+===========================================================================
+CREATE DATABASE AND SCHEMA
+===========================================================================
+
+===========================================================================
+Script Purpose:
+This script creates DataBase named 'DataWarHouse'
+IF there any similiar named DataBases available 
+then this script will DROP the database and recreate a new DataBase.
+
+ADDITIONALLY, This Script has three Schemas : 'bronze', 'silver', 'gold' 
+===========================================================================
+
+===========================================================================
+Warning:
+This Script will permanently delete all DataBases named 'DataWarHouse' if it exist.
+So, before Running this script ensure you have proper backups
+
+===========================================================================
+*/
+-- DROP AND RECREATE 'DATAWARHOUSE' DATABASE
+USE master;
+GO
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name ='DataWarHouse')
+
+BEGIN
+	ALTER DATABASE DataWarHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarHouse;
+
+END;
+
+GO
+-- CREATE 'DataWarHouse' DataBase
+CREATE DATABASE DataWarHouse
+
+GO
+
+USE DataWarHouse;
+
+GO
+
+CREATE SCHEMA bronze;
+
+GO
+
+CREATE SCHEMA silver;
+
+GO
+
+CREATE SCHEMA gold;
+
+GO
+
